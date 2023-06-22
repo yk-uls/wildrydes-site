20ed214839c745f21a1e1c33e83d61f1fe00f2a6
Divyansh Bhardwaj <87605149+Dbhardwaj99@users.noreply.github.com>
2023-06-23 03:18:27 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_fMkul0Fbi', 
+        userPoolClientId: '539dfgrgg7onkeevjvisjb3v47', 
+        region: 'ap-south-1' 
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
