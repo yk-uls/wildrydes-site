3476a655af7ec7e1ee22762bd78cef993e93bc03
Divyansh Bhardwaj <87605149+Dbhardwaj99@users.noreply.github.com>
2023-06-23 14:18:26 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-south-1' 
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://b8hr1whxef.execute-api.ap-south-1.amazonaws.com/prod',
     }
 };
